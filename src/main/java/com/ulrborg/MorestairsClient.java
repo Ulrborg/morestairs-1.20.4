e7be43ca79255171f49674b4ca6f9b5cf6db0742
BuildTools <unconfigured@null.spigotmc.org>
2023-12-13 02:08:20 +0100

    Initial commit

---
+++ src/main/java/com/ulrborg/MorestairsClient.java
@@ -0,0 +1,10 @@
+package com.ulrborg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorestairsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
